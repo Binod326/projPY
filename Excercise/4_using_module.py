64bdb9bf5d1f9fca7409780a8a53cdfd0f5fd7fc
Binod Ubuntu <gbinodmaharjan@gmail.com>
2022-05-04 09:08:12 +0545

    USING MODULE RELATIVE IMPORT

---
+++ Excercise/4_using_module.py
@@ -0,0 +1,10 @@
+#excersice using modules
+
+from gettext import ngettext
+import math
+content = dir(math)
+print(content)
+
+from ...projPY import Name_Game as NG
+content = dir(NG)
+print(NG)
--- using module.py
+++
@@ -1,9 +0,0 @@
-#excersice using modules
-
-import math
-content = dir(math)
-print(content)
-
-'''import Name_Game
-content = dir(Name_Game)
-print(content)'''
