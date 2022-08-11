62861cd1f66ad7dbfd136fd430119f1595011784
Binod <binod.snjoshi@gmail.com>
2022-08-11 08:54:06 +0545

    add string_arrow.py

--- Exercise/1_string_arrow.py
+++ Exercise/1_string_arrow.py
@@ -3,7 +3,7 @@ print ('Oink!..')
 print ('Meow!!...')
 print ('Roar....')
 
-#using sep separator and end to print function
+#using sep separator and end to print function..
 print ("I","am","going","to","draw","an arrow",sep="_",end="_*_")
 print("with the help of codes")
 
