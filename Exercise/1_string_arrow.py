fec7e2ec83732cb19fea70d9a3a5e78af5470611
Binod <binod.snjoshi@gmail.com>
2022-08-11 08:52:01 +0545

    as of 2022-08-11

---
+++ Exercise/1_string_arrow.py
@@ -0,0 +1,20 @@
+print ("Hisssss...")
+print ('Oink!..')
+print ('Meow!!...')
+print ('Roar....')
+
+#using sep separator and end to print function
+print ("I","am","going","to","draw","an arrow",sep="_",end="_*_")
+print("with the help of codes")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+print("    *         *\n   * *       * *\n  *   *     *   *\n *     *   *     *\n***   *** ***   ***\n  *   *     *   *\n  *   *     *   *\n  *****     *****")
+print("        *\n      *   *\n    *       *\n  *           *\n* * *       * * *\n    *       *\n    *       *\n    * * * * *")
