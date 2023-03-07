b1dee27bb08184e878e4eb2656e3efd3fcb8ce07
Binod <binod.snjoshi@gmail.com>
2023-03-07 22:40:02 +0545

    webscrapping

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -13,8 +13,8 @@
 # pip install html5lib
 
 import requests
-from bs4 import BeautifulSoup
-url = "https://codewithharry.com"
+from bs4 import BeautifulSoup  # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+url = "https://www.codewithharry.com/"
 
 # Step 1: Get the HTML
 r = requests.get(url)
@@ -25,36 +25,40 @@ htmlContent = r.content
 soup = BeautifulSoup(htmlContent, 'html.parser')
 # print(soup.prettify)
 
-# Step 3: HTML Tree traveesal
+# Step 3: HTML Tree traversal
 # 1. Tag
 # 2. NavigableString
 # 3. BeautifulSoup
 # 4. Comment
+
+# 1. Using Tags
 title = soup.title
 # print(title)
+# Get the title of the HTML Page
+print(title.string)
+# # finding it's type
 # print(type(title))
 # print(type(title.string))
 # print(type(soup))
 
-# Get the title of the HTML Page
-print(title.string)
-
-# Get all the paragraphs from the page
-paras = soup.find_all('p')
+# # Get all the paragraphs from the page
+# paras = soup.find_all('p')
 # print(paras)
 
-# Get all the anchor tags from the page
-anchors = soup.find_all('a')
+# # Get all the anchor tags from the page
+# anchors = soup.find_all('a')
 # print(anchors)
 
-# Get first element in the HTML page
-print(soup.find('p'))
+# # Get first element in the HTML page. For eg, first element in HTML with tag <p>
+# print(soup.find('p'))
 
-# # Get classes of any element in the HTML page
+# # Get classes of any element in the HTML page. For eg, get classes of first element with tag <p>.
 # print(soup.find('p')['class'])
 
 # # find all the element with class mt-2
-# print(soup.find_all("p", class_="mt-2"))
+# # print(soup.find_all("p", class_="mt-2")) 
+# print(soup.find_all("p", class_="text-gray-700"))
 
 # Get the text from the tags/soup
-print(soup.find('p').get_text)
+print(soup.find('p').get_text())
+print(soup.find('p').string)
